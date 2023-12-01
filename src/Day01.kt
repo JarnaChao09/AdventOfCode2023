5d7c9ec57235645edb916e8005e23eca39cbf145
JarnaChao09 <yunyang4928@gmail.com>
2023-12-01 01:27:46 -0500

    Day 01 Solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    readInput("input/Day01").sumOf {
+        it.split(Regex("\\D")).filter(String::isNotEmpty).flatMap(String::toList).run {
+            "${first()}${last()}".toInt()
+        }
+    }.println()
+    readInput("input/Day01").sumOf {
+        buildList {
+            it.withIndex().forEach { (index, c) ->
+                if (c in '0'..'9') {
+                    add(c - '0')
+                }
+                if (index + 3 <= it.length) {
+                    when (it.slice(index..<index + 3)) {
+                        "one" -> add(1)
+                        "two" -> add(2)
+                        "six" -> add(6)
+                    }
+                }
+                if (index + 4 <= it.length) {
+                    when (it.slice(index..<index+4)) {
+                        "four" -> add(4)
+                        "five" -> add(5)
+                        "nine" -> add(9)
+                    }
+                }
+                if (index + 5 <= it.length) {
+                    when (it.slice(index..<index+5)) {
+                        "three" -> add(3)
+                        "seven" -> add(7)
+                        "eight" -> add(8)
+                    }
+                }
+            }
+        }.let {
+            "${it.first()}${it.last()}".toInt()
+        }
+    }.println()
 }
